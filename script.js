cd91ccfd8089d032db72bbef2e3f9ebc844f5df7
Luiz Guilherme <70903868+LuizGuilherme322@users.noreply.github.com>
2023-11-14 17:18:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var name = prompt("Qual é o seu nome?");
+var dist = prompt("Qual é a distância de anos luz?");
+var anosluz = 9.461e15;
+var anosluzametros = dist * 9.461e15;
+//var anosluzametros = anoluzametros.toFixed(3);
+alert(dist + " em anos luz é " + anosluzametros);
